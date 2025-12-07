b8b688cff086205794220ccd0f43f16ad93e0acb
wutt0019 <wutt0019@flinders.edu.au>
2025-12-07 17:04:32 +1030

    Day7 part 1 complete, also updated gitignore.

--- Day7/part1.cpp
+++ Day7/part1.cpp
@@ -1,6 +1,53 @@
 #include <iostream>
 #include <string>
+#include <vector>
+
+size_t depth = 0;
+size_t width = 0;
+
+void traverse(int i, int j, std::vector<std::string>& matrix) {
+    if (i == depth - 1) {
+        return;
+    }
+
+    if (matrix[i][j] == '.' || matrix[i][j] == '|') {
+        traverse(i+1, j, matrix);
+    } else {
+        if (matrix[i][j] == '~') return;
+
+        matrix[i][j] = '~'; // visited
+        matrix[i-1][j] = '|'; // beam hit
+        traverse(i+1, j+1, matrix);
+        traverse(i+1, j-1, matrix);
+    }
+}
 
 int main() {
+    std::vector<std::string> grid;
 
+    while (!std::cin.eof()) {
+        std::string input;
+        std::cin >> input;
+        grid.push_back(input);
+        depth++;
+    }
+    width = grid[0].size();
+
+    int start = 0;
+    for (int i {}; i < grid[0].size(); i++) {
+        if (grid[0][i] == 'S') {
+            start = i;
+        }
+    }
+    traverse(1, start, grid);
+
+    int count = 0;
+    for (int i {}; i < grid.size(); i++) {
+        for (int j {}; j < grid[i].size(); j++) {
+            if (grid[i][j] == '|') {
+                count++;
+            }
+        }
+    }
+    std::cout << count << std::endl;
 }
